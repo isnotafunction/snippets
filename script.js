f6d068727a1bdf6ecff1d1ad6e9a0a6e7d3d0fd5
isnotafunction <ivana.cosgrove@gmail.com>
2018-02-04 20:03:08 +0000

    sets, gets and clears localStorage

--- script.js
+++ script.js
@@ -1,32 +1,101 @@
 var plus = document.querySelector(".plus");
 var parent = document.querySelector(".container");
-// var clear  = document.querySelector(".clear");
+var x = document.querySelector(".x")
+var clear = document.querySelector('.clear')
+
+var data = (localStorage.getItem("item")) ? JSON.parse(localStorage.getItem("item")):{
+  text:[]
+}
+
+console.log(data.text)
+
+renderDivs()
 
 plus.addEventListener("click", function(){
   createCard();
 })
 
+parent.addEventListener("click", function(e){
+  if(e.target.matches("button.x")){
+    console.log(e.target)
+  var element = e.path[1].firstElementChild.innerText
+  console.log(element)
+  console.log(typeof(element))
+  var index = data.text.indexOf(element.trim())
+  console.log(index)
+  var splicedEle = data.text.splice(index, 1)
+  console.log(splicedEle)
+  updateStorage()
+  console.log(data.text)
+ }
+})
+
 
 function createCard(){
   var card = document.createElement("div")
-  var pre = document.createElement("pre")
-  var content = document.createElement("code")
-  var x = document.createElement("button")
+  var content = document.createElement("p")
+  var xButton = document.createElement("button")
   card.setAttribute("class","card")
   content.setAttribute("contenteditable", "true")
   content.setAttribute("id", "content")
   content.innerText = "code snippet..."
-  x.setAttribute("class", "x")
-  x.setAttribute("onClick", "this.parentNode.parentNode.removeChild(this.parentNode)")
-  x.innerText = "x"
-  card.appendChild(pre)
-  pre.appendChild(content)
-  card.appendChild(x)
+  xButton.setAttribute("class", "x")
+  xButton.setAttribute("onClick", "this.parentNode.parentNode.removeChild(this.parentNode)")
+  xButton.innerText = "x"
+  card.appendChild(content)
+  card.appendChild(xButton)
   parent.appendChild(card)
  }
 
+parent.addEventListener("keyup", saveEdits)
+
+var timeout;
+//save edits to object
+function saveEdits(e) {
+    clearTimeout(timeout);
+    timeout = setTimeout(function(){
+      var text = e.target.innerText;
+      data.text.push(text.trim());
+      updateStorage()
+    }, 2000)
+  }
+
+//save to localStorage
+function updateStorage(){
+  localStorage.setItem("item", JSON.stringify(data))
+}
+
+//render HTML from localStorage
+function renderDivs(){
+  if (!data.text.length){return}
+  for(var i=0; i<data.text.length;i++){
+    var textToInsert = data.text[i];
+    var cardToRender =
+    `<div class="card">
+      <p id="content" contenteditable="true" > ${textToInsert}</p>
+      <button class="x" onClick="this.parentNode.parentNode.removeChild(this.parentNode)">x</button>
+    </div>
+    `
+    parent.insertAdjacentHTML("beforeend", cardToRender)
+  }
+}
 
 
- // clear.addEventListener("click", function(){
- //   localStorage.clear();
- // })
+
+clear.addEventListener("click", function(){
+  window.localStorage.clear()
+  document.location.reload(true)
+})
+
+ // Array.from(p).forEach(function(el){
+ // for(var i=0; i<p.length; i++){
+  //  console.log(el.innerText)
+  //  console.log("end")
+ // }
+  // data.text.push(userVersion)
+  // console.log(data)
+  // localStorage.setItem("items", JSON.stringify(data))
+ // }
+
+
+// console.log(data.text)
