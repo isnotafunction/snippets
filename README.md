8f3f74ba89a93d90bd7f5985c445ee802de2dca1
isnotafunction <ivana.cosgrove@gmail.com>
2018-02-04 11:57:52 +0000

    reworked the way of adding html, added <code> as content and removed title

--- README.md
+++ README.md
@@ -3,10 +3,13 @@
 User journey:
 As a user I want to be able to:
 
-* click on a button to add a new flesh-card to the deck;
-* add a flash-card's title
-* click on the flesh-card to "flip" it and add it's content
+* click on a button to add a new flesh-card to the deck;√
+* add/edit title √
+* add/edit card's content √
+* click on a button to delete all storage √
+* click on a button to delete a card;√
+* click on the flesh-card to "flip" it;?
+* save content in browser
 * choose which side of flash-card is visible (by clicking and flipping)
-* click on a button to delete a card;
-* edit card's content
-* embed images
+* change background colour?
+* change card's colour?
--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-var button = document.querySelector(".plus");
+var plus = document.querySelector(".plus");
 var parent = document.querySelector(".container");
+// var clear  = document.querySelector(".clear");
 
-button.addEventListener("click", function(){
+plus.addEventListener("click", function(){
   createCard();
 })
 
 
 function createCard(){
- var card =
- `<div class="card">
- <h4 contenteditable="true">Title</h4>
- <p class="text" contenteditable="true">Edit me...</p>
- <button class="x" onClick="this.parentNode.parentNode.removeChild(this.parentNode);">x</button>
- </div>`;
- parent.insertAdjacentHTML("beforeend", card);
-}
+  var card = document.createElement("div")
+  var pre = document.createElement("pre")
+  var content = document.createElement("code")
+  var x = document.createElement("button")
+  card.setAttribute("class","card")
+  content.setAttribute("contenteditable", "true")
+  content.setAttribute("id", "content")
+  content.innerText = "code snippet..."
+  x.setAttribute("class", "x")
+  x.setAttribute("onClick", "this.parentNode.parentNode.removeChild(this.parentNode)")
+  x.innerText = "x"
+  card.appendChild(pre)
+  pre.appendChild(content)
+  card.appendChild(x)
+  parent.appendChild(card)
+ }
+
+
+
+ // clear.addEventListener("click", function(){
+ //   localStorage.clear();
+ // })
