271d72cd5002bacb671b387d4bece0687af3bc8b
isnotafunction <ivana.cosgrove@gmail.com>
2018-02-07 17:03:30 +0000

    added Unique Identifier(uid) to each card

--- script.js
+++ script.js
@@ -3,39 +3,38 @@ var parent = document.querySelector(".container");
 var x = document.querySelector(".x")
 var clear = document.querySelector('.clear')
 
-var data = (localStorage.getItem("item")) ? JSON.parse(localStorage.getItem("item")):{
-  text:[]
-}
+var data = (localStorage.getItem("item")) ? JSON.parse(localStorage.getItem("item")):[]
 
-console.log(data.text)
+console.log(data)
 
 renderDivs()
 
-plus.addEventListener("click", function(){
-  createCard();
-})
+plus.addEventListener("click", createCard)
 
+//deleting from the storage
 parent.addEventListener("click", function(e){
   if(e.target.matches("button.x")){
-  var element = e.path[1].firstElementChild.innerText
-  console.log(element)
-  console.log(typeof(element))
-  var index = data.text.indexOf(element.trim())
-  console.log(index)
-  var splicedEle = data.text.splice(index, 1)
-  console.log(splicedEle)
+  // var element = e.path[1].firstElementChild.innerText
+  var uid = e.path[1].firstElementChild.id;
+  console.log("this is uid "+ uid)
+  var index = data.findIndex(x => x.id == uid)
+  console.log("index to delete "+ index)
+  var splicedEle = data.splice(index, 1)
+  console.log("splicedElement " + splicedEle)
   updateStorage()
-  console.log(data.text)
+  console.log(data)
  }
 })
 
-
+var uid = (localStorage.getItem("uid"))? JSON.parse(localStorage.getItem("uid")): 0
 function createCard(){
+  uid++
   var card = document.createElement("div")
   var content = document.createElement("textarea")
   var xButton = document.createElement("button")
   card.setAttribute("class","card")
-  content.setAttribute("id", "content")
+  content.setAttribute("class", "content")
+  content.setAttribute("id", `u${uid}`)
   content.setAttribute("value", "")
   content.innerText = "snippets..."
   xButton.setAttribute("class", "x")
@@ -44,18 +43,27 @@ function createCard(){
   card.appendChild(content)
   card.appendChild(xButton)
   parent.appendChild(card)
+  localStorage.setItem("uid", JSON.stringify(uid))
  }
 
 parent.addEventListener("keyup", saveEdits)
 
 var timeout;
-//save edits to object
+//save edits to array and update storage
 function saveEdits(e) {
     clearTimeout(timeout);
     timeout = setTimeout(function(){
-      var text = e.target.value;
+      var text = e.target.value
       console.log(e.target.value)
-      data.text.push(text.trim());
+      var uid = e.target.id
+      console.log(e.target.id)
+      var index = data.findIndex(x => x.id == uid)
+      if(index == -1){
+          data.push({"text":text.trim(), "id": uid});
+      } else {
+        data[index] = {"text":text.trim(), "id": uid}
+      }
+     console.log(data)
       updateStorage()
     }, 4000)
   }
@@ -67,12 +75,12 @@ function updateStorage(){
 
 //render HTML from localStorage
 function renderDivs(){
-  if (!data.text.length){return}
-  for(var i=0; i<data.text.length;i++){
-    var textToInsert = data.text[i];
+  if (!data.length){return}
+  for(var i=0; i<data.length;i++){
+    var textToInsert = data[i].text;
     var cardToRender =
     `<div class="card">
-      <textarea id="content" value="${textToInsert}"> ${textToInsert}</textarea>
+      <textarea class="content" value="${textToInsert}" id="${data[i].id}"> ${textToInsert}</textarea>
       <button class="x" onClick="this.parentNode.parentNode.removeChild(this.parentNode)">x</button>
     </div>
     `
